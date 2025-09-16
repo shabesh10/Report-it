2add03d3fe8e33d8c204e508ac003aceeaed6f67
Shabeshvaran <shabeshvaran@gmail.com>
2025-09-16 17:05:55 +0530

    project archi

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,8 @@
-import "./global.css"
-import * as React from "react"
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu"
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="p-4">
-      <NavigationMenu>
-        <NavigationMenuList className="flex space-x-4">
-
-          {/* Home */}
-          <NavigationMenuItem>
-            <NavigationMenuLink className="px-3 py-2 rounded hover:bg-gray-100">
-              Home
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-
-          {/* Report Dropdown */}
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="px-3 py-2 rounded hover:bg-gray-100">
-              Report
-            </NavigationMenuTrigger>
-            <NavigationMenuContent className="p-4 bg-white border rounded shadow-lg mt-1">
-              <ul className="grid gap-2">
-                <NavigationMenuLink className="px-3 py-2 rounded hover:bg-gray-100">
-                  Create Reports
-                </NavigationMenuLink>
-                <NavigationMenuLink className="px-3 py-2 rounded hover:bg-gray-100">
-                  All Reports
-                </NavigationMenuLink>
-                <NavigationMenuLink className="px-3 py-2 rounded hover:bg-gray-100">
-                  Your Reports
-                </NavigationMenuLink>
-                <NavigationMenuLink className="px-3 py-2 rounded hover:bg-gray-100">
-                  Map
-                </NavigationMenuLink>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-
-          {/* Map */}
-          <NavigationMenuItem>
-            <NavigationMenuLink className="px-3 py-2 rounded hover:bg-gray-100">
-              Map
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
+    <div>App</div>
   )
 }
 
